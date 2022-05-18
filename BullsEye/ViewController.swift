c1fe8f2536ef82762271fbd4004897ee148311e5
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-05-18 23:56:04 +0400

    Init commit

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var slider: UISlider!
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currentValue = lroundf(slider.value)
     }
 
+    @IBAction func showAlert() {
+        let message = "Your slider value is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 }
 
