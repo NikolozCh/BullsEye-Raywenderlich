199b380a38476c4095911afac23354e97e3361c4
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-05-19 00:33:55 +0400

    Finished chapter 04-Outlets

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,27 +10,45 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var slider: UISlider!
-    var currentValue: Int = 50
+    @IBOutlet var targetValueOutlet: UILabel!
+    var currentValue: Int = 0
+    var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        currentValue = lroundf(slider.value)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let message = "Your slider value is: \(currentValue)"
-        
+        let message = "Your slider value is: \(currentValue)" +
+                     "\nThe target value is: \(targetValue)"
+        // Creating UIAlertController to notify user
         let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
-        
+        // Creating UIAlertAction to be added in UIAlertController later
         let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
+        // Adding action to alert
         alert.addAction(alertAction)
+        // Presenting the alert
         present(alert, animated: true, completion: nil)
+        // Hit Me! pressed, after all calculations, we are starting a new round
+        startNewRound()
     }
 
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        currentValue = 50
+        slider.value = Float(currentValue)
+        // Generating random number on start of the application and setting it to the targetValue
+        targetValue = Int.random(in: 1...100)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        // Setting the Outlet 'targetValue's value
+        targetValueOutlet.text = String(targetValue)
     }
 }
 
